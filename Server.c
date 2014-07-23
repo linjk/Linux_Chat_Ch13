c91ef9a47a589dccc4cce863d8bceb3a6b86fc17
linjk <linjk121@163.com>
2014-07-23 11:12:18 +0800

    Update Server.c

--- Server.c
+++ Server.c
@@ -1,4 +1,17 @@
 /******************************************************
- *
- *
+ * File : Server.c
+ * Des  : Chat - Server.c Base On BSD_Socket
+ * Date : 2014-07-24
+ * By   : LinJK
 ******************************************************/
+#include <stdio.h>
+
+#define MAXBUF 1024
+
+int main(int argc, char *argv[], char *argv[]){
+    
+
+    return 0;  
+}
+
+
